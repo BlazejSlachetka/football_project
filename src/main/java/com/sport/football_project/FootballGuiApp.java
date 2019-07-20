e9be6816dce76c47732a28ffce83aff3f401353d
Błażej Ślachetka <blazej.slachetka@gmail.com>
2019-07-20 12:52:52 +0200

    V5

--- src/main/java/com/sport/football_project/FootballGuiApp.java
+++ src/main/java/com/sport/football_project/FootballGuiApp.java
@@ -2,12 +2,13 @@ package com.sport.football_project;
 
 
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Route("football")
-public class FootballGuiApp {
+public class FootballGuiApp extends VerticalLayout {
 
 @Autowired
     public FootballGuiApp (FootballClient footballClient){
