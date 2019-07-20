30aa5cee9281673d17c8d2cb1214a97334feac88
Błażej Ślachetka <blazej.slachetka@gmail.com>
2019-07-20 12:08:09 +0200

    V3 - web security.

---
+++ src/main/java/com/sport/football_project/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.sport.football_project;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+
+    @Configuration
+    @EnableOAuth2Sso
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/football").authenticated()
+                    .antMatchers("/hello").permitAll()
+                    .and()
+                    .csrf().disable();
+        }
+    }
+
