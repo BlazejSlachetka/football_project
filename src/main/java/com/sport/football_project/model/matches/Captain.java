0468ef260b4cad02a1e86d431bb3ef901c6772fc
Błażej Ślachetka <blazej.slachetka@gmail.com>
2019-07-21 12:12:49 +0200

    Zastosowanao poprawki dla matches

--- src/main/java/com/sport/football_project/model/matches/Captain.java
+++ src/main/java/com/sport/football_project/model/matches/Captain.java
@@ -1,5 +1,9 @@
 package com.sport.football_project.model.matches;
 
+
+import lombok.Data;
+
+@Data
 public class Captain {
     Long id;
     String name;
--- src/main/java/com/sport/football_project/model/matches/Penalties.java
+++ src/main/java/com/sport/football_project/model/matches/Penalties.java
@@ -1,5 +1,8 @@
 package com.sport.football_project.model.matches;
 
+import lombok.Data;
+
+@Data
 public class Penalties {
     String homeTeam;
     String awayTeam;
