b7416d209581c7a5e1e8b847b144bdc2da9bc6ac
Błażej Ślachetka <blazej.slachetka@gmail.com>
2019-07-21 10:04:31 +0200

    V6

---
+++ src/main/java/com/sport/football_project/Area.java
@@ -0,0 +1,10 @@
+package com.sport.football_project;
+
+import lombok.Data;
+
+@Data
+public class Area {
+
+    Long id;
+    String name;
+}
---
+++ src/main/java/com/sport/football_project/Competition.java
@@ -0,0 +1,20 @@
+package com.sport.football_project;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Competition {
+
+    Long id;
+    Area area;
+    String name;
+    String code;
+    String plan;
+    Season currentSeason;
+    Long numberOfAvailableSeasons;
+    LocalDateTime lastUpdate;
+
+
+}
