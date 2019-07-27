9bdee53ea48bb221d1c25e6f5d1bf0c1f69df880
Błażej Ślachetka <blazej.slachetka@gmail.com>
2019-07-27 12:19:22 +0200

    Poprawiono architekturę programu.

--- src/main/java/com/sport/football_project/gui/FootballGuiApp.java
+++ src/main/java/com/sport/football_project/gui/FootballGuiApp.java
@@ -2,14 +2,13 @@ package com.sport.football_project.gui;
 
 import com.sport.football_project.clients.*;
 import com.sport.football_project.common.*;
-import com.sport.football_project.model.teams.Team;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
-;
+;import java.awt.*;
 
 
 @Route("football")
@@ -28,11 +27,13 @@ public class FootballGuiApp extends VerticalLayout {
         Players players = playersClient.getPlayersFormFormApi();
         Teams teams = teamsClient.getTeamsFormFormApi();
         Competitions competitions = competitionsClient.getCompetitionsFormFormApi();
+        ;
+        Label label = new Label("Podaj Nazwę Klubu");
+        add(label);
+        TextField textField = new TextField();
+        add(textField);
 
-        Label labelMatches = new Label(matchesClient.getMatchesFormFormApi().getFilters().getPermission());
-        add(labelMatches);
-
-
+        label.setText("Klub" + textField.equals(teamsClient));
 
 
 
